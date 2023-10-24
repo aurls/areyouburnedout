7c495159960f0507dfa4cf5d083f08c8a383e5ad
Vladislav Taranov <vladislav.taranov@live.com>
2023-10-24 03:50:58 +0300

    feat: add Form layout

---
+++ src/client/layouts/Form/Form.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useSelector, useDispatch } from '../../store';
+import { root } from '../../slices';
+import FormItem, { Type } from '../FormItem';
+
+import './Form.scss';
+
+const Form: React.FC = () => {
+  const value = useSelector((state) => state.root.value);
+
+  const dispatch = useDispatch();
+
+  const setValue = (valueToMerge) => {
+    dispatch(root.setValue({ ...value, ...valueToMerge }));
+  };
+
+  const formItems = [
+    {
+      type: Type.Number,
+      id: 'age',
+      title: 'Age:',
+      min: 18,
+      max: 99,
+      onChange: (age) => {
+        setValue({ age });
+      },
+      style: { width: '50%' }
+    },
+    {
+      type: Type.Select,
+      id: 'gender',
+      title: 'Gender:',
+      placeholder: 'Select gender',
+      options: [
+        { id: 'Male', title: 'Male' },
+        { id: 'Female', title: 'Female' }
+      ],
+      onChange: (gender) => {
+        setValue({ gender });
+      },
+      style: { width: '50%' }
+    }
+  ];
+
+  return (
+    <div className="form">
+      {formItems.map((item) => (
+        <FormItem key={item.id} {...item} value={value[item.id]} />
+      ))}
+    </div>
+  );
+};
+
+export default Form;
