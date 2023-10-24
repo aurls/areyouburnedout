f0e40b30750a8cdc9f0c13be6325b9070507404a
Vladislav Taranov <vladislav.taranov@live.com>
2023-10-24 06:50:34 +0300

    feat(Form): add Buttons

--- src/client/layouts/Form/Form.tsx
+++ src/client/layouts/Form/Form.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { useSelector, useDispatch } from '../../store';
 import { root } from '../../slices';
 import FormItem, { Type } from '../FormItem';
+import Button from '../../components/Button';
 
 import './Form.scss';
 
 const Form: React.FC = () => {
   const value = useSelector((state) => state.root.value);
+  const prediction = useSelector((state) => state.root.prediction);
+  const fetching = useSelector((state) => state.root.fetching);
+  const error = useSelector((state) => state.root.error);
 
   const dispatch = useDispatch();
 
@@ -14,22 +18,29 @@ const Form: React.FC = () => {
     dispatch(root.setValue({ ...value, ...valueToMerge }));
   };
 
+  const onReset = () => {
+    dispatch(root.resetValue());
+  };
+
+  const onSubmit = () => {
+    dispatch(root.postParams(value));
+  };
+
   const formItems = React.useMemo(() => [
     {
       type: Type.Number,
       id: 'age',
-      title: 'Age:',
+      title: 'Your age:',
       min: 18,
       max: 99,
       onChange: (age) => {
         setValue({ age });
-      },
-      style: { width: '50%' }
+      }
     },
     {
       type: Type.Select,
       id: 'gender',
-      title: 'Gender:',
+      title: 'Your gender:',
       placeholder: 'Select gender',
       options: [
         { id: 'Male', title: 'Male' },
@@ -37,22 +48,38 @@ const Form: React.FC = () => {
       ],
       onChange: (gender) => {
         setValue({ gender });
-      },
-      style: { width: '50%' }
+      }
     }
   ], []);
 
   return (
     <>
       <h1 className="form__title">
-        Are You Burned Out?? Learn It With AI
+        Are You <span className="form__title-accent">Burned Out</span>??
+        <br />
+        <span className="form__title-secondary">Learn It With AI &gt;&gt;</span>
       </h1>
 
       <form className="form">
         {formItems.map((item) => (
-          <FormItem key={item.id} {...item} value={value[item.id]} />
+          <FormItem
+            key={item.id}
+            {...item}
+            value={value[item.id]}
+            disabled={fetching}
+          />
         ))}
       </form>
+
+      <div className="form__actions">
+        <Button primary onClick={onSubmit} title="Ask AI" disabled={fetching}>
+          &gt;&gt; Ask AI &lt;&lt;
+        </Button>
+
+        <Button onClick={onReset} title="Reset" disabled={fetching}>
+          Reset
+        </Button>
+      </div>
     </>
   );
 };
