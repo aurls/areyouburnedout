d1aeb668b35f8d77392d9b324213cae0cdae9df1
Vladislav Taranov <vladislav.taranov@live.com>
2023-10-24 06:17:02 +0300

    feat(slices): add postParams

--- src/client/slices/root.ts
+++ src/client/slices/root.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import server from '../services/server';
 import { Value } from '../types';
 
 const defaultValue: Value = {
@@ -7,12 +8,27 @@ const defaultValue: Value = {
 }
 
 interface RootState {
-  value: Value
+  value: Value,
+  prediction: boolean | null,
+  fetching: boolean,
+  error: boolean
 }
 
 const initialState: RootState = {
-  value: defaultValue
+  value: defaultValue,
+  prediction: null,
+  fetching: true,
+  error: false
 };
+
+const postParams = createAsyncThunk(
+  'root/postParams',
+  async (params: Value) => {
+    const prediction = await server.postParams(params);
+
+    return prediction;
+  }
+);
 
 const root = createSlice({
   name: 'root',
@@ -20,11 +36,35 @@ const root = createSlice({
   reducers: {
     setValue: (state, action: PayloadAction<any>) => {
       state.value = action.payload;
-    }
+      state.prediction = null;
+      state.error = false;
+    },
+    resetValue: (state) => {
+      state = initialState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(postParams.pending, (state) => {
+        state.prediction = null;
+        state.fetching = true;
+        state.error = false;
+      })
+      .addCase(postParams.fulfilled, (state, action) => {
+        state.prediction = action.payload;
+        state.fetching = false;
+        state.error = false;
+      })
+      .addCase(postParams.rejected, (state) => {
+        state.prediction = null;
+        state.fetching = false;
+        state.error = true;
+      })
   }
 });
 
 export default {
   ...root.actions,
+  postParams,
   reducer: root.reducer
 };
