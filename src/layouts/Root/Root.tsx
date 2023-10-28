104bcd0d5fdb8a22174c6110080fb30589a76fc0
Vladislav Taranov <vladislav.taranov@live.com>
2023-10-28 16:19:25 +0300

    feat: add router

--- src/layouts/Root/Root.tsx
+++ src/layouts/Root/Root.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Notifications from '../../components/Notifications';
 import Form from '../Form';
+import Prediction from '../Prediction';
 
 import './Root.scss';
 
+const router = createBrowserRouter([
+  {
+    path: '/:id',
+    element: <Prediction />
+  },
+  {
+    path: '/',
+    element: <Form />
+  }
+]);
+
 const Root: React.FC = () => {
   return (
-    <div className="root">
-      <Form />
-    </div>
+    <>
+      <Notifications />
+
+      <div className="root">
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 };
 
